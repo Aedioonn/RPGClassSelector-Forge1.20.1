a48bb7118d31dc89afe6a07fce66fcd620ccbd8b
Aedioonn <aedion.2121@gmail.com>
2025-10-24 15:30:12 +0200

    Create ClassCommand.java

---
+++ src/main/java/com/example/rpgclassselector/config/src/main/java/com/example/rpgclassselector/ClassCommand.java
@@ -0,0 +1,7 @@
+package com.example.rpgclassselector;
+
+// Placeholder: sem přijde registrace příkazů pomocí Brigadier
+// (např. /class <hráč> <role> a /class reset <hráč>)
+// Zatím slouží jako struktura, aby build proběhl správně.
+public class ClassCommand {
+}
